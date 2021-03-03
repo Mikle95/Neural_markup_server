2cad0a24ac41708f2631a74c7c51fdd56d4efd31
mikle95 <mikle9570221@mail.ru>
2021-03-03 13:39:59 +0300

    Added Login and Via front

--- app/models.py
+++ app/models.py
@@ -1,10 +1,29 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     # email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    Markups = db.relationship('Markup', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User: {}, Id: {}, Online: {}>'.format(self.username, self.id, self.is_authenticated)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Markup(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
-from app import app
-from flask import request
+from app import app, forms
+from app.models import User
+from flask import Flask, jsonify, request, render_template, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
+# https://www.youtube.com/watch?v=9MHYHgh4jYc
+
 
 @app.route('/')
 @app.route('/check')
 def index():
-    return "The server is running!"
+    if current_user.is_authenticated:
+        return "You are authenticated, {}!".format(current_user.username)
+    else:
+        return "You are not authenticated!"
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -16,3 +25,44 @@ def shutdown_server():
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
+
+
+
+
+@app.route('/login/', methods=["POST", "GET"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        name = request.form["nm"]
+        password = request.form["pw"]
+        user = User.query.filter_by(username=name).first()
+        if user is None or not user.check_password(password):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('via')
+        return redirect(next_page)
+    else:
+        return render_template("login.html")
+
+@app.route('/logout/')
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/<text>')
+def echo(text):
+    return render_template("index.html", content=text)
+
+@app.route('/via')
+@login_required
+def via():
+    return render_template("_via_video_annotator.html")
+
+
+
+
